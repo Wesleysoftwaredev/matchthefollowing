ee5cd8c7d47ba98694b85b8303bd675711475904
Wesleysoftwaredev <aghayedow@gmail.com>
2025-09-03 19:11:30 +0100

    Add files via upload

--- flappy.py
+++ flappy.py
@@ -19,6 +19,52 @@ bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
 
 run = True
+
+class bird(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range (1,4):
+            img = pygame.image.load(f"img/bird{num}.png")
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center=[x,y]
+        self.vel = 0
+        self.clicked = False
+
+    def update(self):
+        if flying == True:
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel=8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
+        if game_over == False:
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+            #handleanims
+            flap_cooldown = 5
+            self.counter += 1
+
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+                    self.image = self.images[self.index]
+
+            
+
+
+
+
 while run:
 
     clock.tick(fps)
@@ -35,5 +81,3 @@ while run:
     pygame.display.update()
 
 pygame.quit
-
-
