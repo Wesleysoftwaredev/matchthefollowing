5f738bcc45b83ba9d940fcec5ee26f0239a49846
Wesleysoftwaredev <aghayedow@gmail.com>
2025-09-10 19:14:50 +0100

    Add files via upload

--- flappy.py
+++ flappy.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 
 pygame.init()
@@ -10,15 +11,38 @@ screen_width = 864
 screen_height = 936
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.display.set_caption('Flappy Bird')
+pygame.display.set_caption('Flappy Bird')
+
+font = pygame.font.SysFont('Bauhaus 93', 60)
+white = (255, 255, 255)
 
 ground_scroll = 0
 scroll_speed = 4
+flying = False
+game_over = False
+pipe_gap = 150
+pipe_frequency = 1500 #ms
+last_pipe = pygame.time.get_ticks() - pipe_frequency
+score = 0
+pass_pipe = False
+
+
 
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
+button_img = pygame.image.load('img/restart.png')
 
-run = True
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(sreen_height / 2)
+    score = 0
+    return score
+
 
 class bird(pygame.sprite.Sprite):
 
@@ -58,25 +82,65 @@ class bird(pygame.sprite.Sprite):
                 self.index += 1
                 if self.index >= len(self.images):
                     self.index = 0
-                    self.image = self.images[self.index]
+                self.image = self.images[self.index]
+            self.image = pygame.transform.rotate(self.images[self.index], self.vel * -2)
+        else:
+            self.images = pygame.transform.rotate(self.images[self.index], -90)
 
-            
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/pipe.png")
+        self.rect = self.image.get_rect()
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+        if position == -1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.topleft = [x, y - int(pipe_gap / 2)]
+
+    
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0:
+            self.kill()
+
+class Button():
 
 
+pipe_group = pygame.sprite.Group()
+bird_group = pygame.sprite.Group()
 
+flappy = Bird(100, int(screen_height / 2)).
+bird_group.add(flappy)
 
+button = Button(screen_width // 2 - 50, screen_height // 2-100, button_img)
+
+run=True               
 while run:
 
     clock.tick(fps)
+
     screen.blit(bg,(0, 0))
+    pipe_group.draw(screen)
+    bird_group.draw(screen)
+    bird_group.update()
+
     screen.blit(ground_img, (ground_scroll, 760))
+
+if len(pipe_group)>0:
+    if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left\
+        and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right\
+        and pass_pipe == False:
+        pass_pipe = True
+
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 35:
         ground_scroll=0
 
     for event in pygame.event.get():
-        if event.type==pygame.QUIT
-        run=False
+        if event.type==pygame.QUIT:
+            run=False
 
     pygame.display.update()
 
