d860358533688118eb6ada0cbd5384903ce21d7f
Wesleysoftwaredev <aghayedow@gmail.com>
2025-09-27 12:02:47 +0100

    Add files via upload

--- class.py
+++ class.py
@@ -1,4 +1,4 @@
-class Dog:
+"""class Dog:
     def __init__(self,name,breed):
         self.name = name
         self.breed = breed
@@ -10,4 +10,20 @@ dog1 = Dog('Will','Labrodor')
 dog2 = Dog('Luna','German Sheperd')
 
 dog1.bark()
-dog2.bark()
+dog2.bark()"""
+
+#HW
+
+class Students:
+    def __init__(self,name,year):
+        self.name = name
+        self.year = year
+
+    def learn(self):
+        print(f'{self.name} is learning')
+
+student1 = Students('David','Year 9')
+student2 = Students('Jason','Year 8')
+
+student1.learn()
+student2.learn()
